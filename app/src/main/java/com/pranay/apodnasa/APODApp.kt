39719bcc7843e05b9c457abb4cbba1db912944d5
pranaypatel512 <www.pranay1337.com@gmail.com>
2022-07-22 12:21:08 +0530

    Created api service class and app class

---
+++ app/src/main/java/com/pranay/apodnasa/APODApp.kt
@@ -0,0 +1,8 @@
+package com.pranay.apodnasa
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class APODApp:Application() {
+}
---
+++ app/src/main/java/com/pranay/apodnasa/data/remote/api/ApiService.kt
@@ -0,0 +1,24 @@
+
+package com.pranay.apodnasa.data.remote.api
+
+import com.pranay.apodnasa.BuildConfig
+import com.pranay.apodnasa.data.remote.api.ApiService.Companion.NASA_API_URL
+import com.pranay.apodnasa.model.APODListResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * Service to fetch pictures using nasa end point [NASA_API_URL].
+ */
+interface ApiService {
+
+    @GET("apod")
+    suspend fun getAPODPictures(@Query("start_date") startDate: String,
+                         @Query("end_date") endDate: String,
+                         @Query("api_key") apiKey: String= BuildConfig.API_KEY): Response<APODListResponse>
+
+    companion object {
+        const val NASA_API_URL = "https://api.nasa.gov/planetary/"
+    }
+}
