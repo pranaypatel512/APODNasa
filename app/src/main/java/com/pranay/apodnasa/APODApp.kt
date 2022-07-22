390fd6b6bf5c78425e8ae3ecee99a62c17a24529
pranaypatel512 <www.pranay1337.com@gmail.com>
2022-07-22 19:18:17 +0530

    Added app class related code for work manager

--- app/src/main/java/com/pranay/apodnasa/APODApp.kt
+++ app/src/main/java/com/pranay/apodnasa/APODApp.kt
@@ -1,8 +1,19 @@
 package com.pranay.apodnasa
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class APODApp:Application() {
+class APODApp : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration() =
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 }
