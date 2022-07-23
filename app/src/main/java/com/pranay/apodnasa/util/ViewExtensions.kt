a3c7c6975fc42e1dfee43c6abd48735e0f35539d
pranaypatel512 <www.pranay1337.com@gmail.com>
2022-07-23 10:52:23 +0530

    :zap: added new view extensions

--- app/src/main/java/com/pranay/apodnasa/util/ViewExtensions.kt
+++ app/src/main/java/com/pranay/apodnasa/util/ViewExtensions.kt
@@ -1,10 +1,24 @@
 package com.pranay.apodnasa.util
 
-import android.content.Context
+import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 
-fun Fragment.showToast(message:String,length:Int=Toast.LENGTH_SHORT){
-    Toast.makeText(requireContext(),message,length).show()
+fun Fragment.showToast(message: String, length: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(requireContext(), message, length).show()
+}
+
+/**
+ * Hide View
+ */
+fun View.hide() {
+    visibility = View.GONE
+}
+
+/**
+ * Show View
+ */
+fun View.show() {
+    visibility = View.VISIBLE
 }
