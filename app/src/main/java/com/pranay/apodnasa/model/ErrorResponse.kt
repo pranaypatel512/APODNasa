81c595175ff82d2229b428c0757a785af9c7eddc
pranaypatel512 <www.pranay1337.com@gmail.com>
2022-07-23 11:02:52 +0530

    :zap: Update error response as per new error

--- app/src/main/java/com/pranay/apodnasa/model/ErrorResponse.kt
+++ app/src/main/java/com/pranay/apodnasa/model/ErrorResponse.kt
@@ -2,10 +2,14 @@ package com.pranay.apodnasa.model
 
 
 import com.google.gson.annotations.SerializedName
-
+//{"code":400,"msg":"Date must be between Jun 16, 1995 and Jul 22, 2022.","service_version":"v1"}
 data class ErrorResponse(
     @SerializedName("error")
-    var error: Error? = null
+    var error: Error? = null,
+    @SerializedName("code")
+    var code: String? = null,
+    @SerializedName("msg")
+    var message: String? = null
 ) {
     data class Error(
         @SerializedName("code")
