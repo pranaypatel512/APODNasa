85d44f1a970747200c0914e4695b4d5ecfec7797
pranaypatel512 <www.pranay1337.com@gmail.com>
2022-07-23 11:03:53 +0530

    :zap: Added new dates related extensions

--- app/src/main/java/com/pranay/apodnasa/util/DateExtensions.kt
+++ app/src/main/java/com/pranay/apodnasa/util/DateExtensions.kt
@@ -1,18 +1,36 @@
 package com.pranay.apodnasa.util
 
+import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.*
 
 /**
  * give two date string from given days different to current date
  */
-fun getTwoDates(format:String="yyyy-MM-dd",beforeDays:Long=7):Pair<String,String>?{
+fun getTwoDates(format: String = "yyyy-MM-dd", beforeDays: Long = 7): Pair<String, String>? {
     return try {
         val formatter = DateTimeFormatter.ofPattern(format)
-        val firstDate = formatter.format(LocalDate.now())
-        val secondDate = formatter.format(LocalDate.now().minusDays(beforeDays))
-        Pair(secondDate,firstDate)
-    }catch (ex:Exception){
+        val endDate = LocalDate.now().minusDays(1)
+        val firstDate = formatter.format(endDate)
+        val secondDate = formatter.format(endDate.minusDays(beforeDays))
+        Pair(secondDate, firstDate)
+    } catch (ex: Exception) {
         null
     }
+}
+
+/**
+ * Convert String date to Date object
+ */
+fun String.toDate(): Date? {
+    return SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).parse(this) ?: null
+}
+
+/**
+ * To convert date into formatted string date
+ */
+fun Date?.formatDate(): String? {
+    if (this == null) return null
+    return SimpleDateFormat("dd MMM, yyyy", Locale.getDefault()).format(this)
 }
