558675bdca4e4ccd7c55f42cd125dc1a0c50f489
pranaypatel512 <www.pranay1337.com@gmail.com>
2022-07-23 18:23:20 +0530

    :sparkles: Integrated pictures details in details screen

--- app/src/main/java/com/pranay/apodnasa/ui/PictureDetailsFragment.kt
+++ app/src/main/java/com/pranay/apodnasa/ui/PictureDetailsFragment.kt
@@ -1,14 +1,28 @@
 package com.pranay.apodnasa.ui
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.navGraphViewModels
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 import com.pranay.apodnasa.R
 import com.pranay.apodnasa.databinding.FragmentPictureDetailsBinding
+import com.pranay.apodnasa.model.APODPictureItem
+import com.pranay.apodnasa.model.State
+import com.pranay.apodnasa.util.*
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -17,27 +31,87 @@ import dagger.hilt.android.AndroidEntryPoint
 class PictureDetailsFragment : Fragment() {
 
     private var _binding: FragmentPictureDetailsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private val picturesViewModel: PicturesViewModel by navGraphViewModels(R.id.nav_graph) {
+        defaultViewModelProviderFactory
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentPictureDetailsBinding.inflate(inflater, container, false)
         return binding.root
 
     }
 
+    private fun observePicturesPosts() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                picturesViewModel.selectedItem.collect { state ->
+                    when (state) {
+                        is State.Loading -> {}
+                        is State.Success -> {
+                            setUpUi(state.data)
+                        }
+                        is State.Error -> {
+                            showToast(state.message)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setUpUi(apodPictureItem: APODPictureItem) {
+        binding.apply {
+            progressBarView.progressBarView.show()
+            textViewPhotoTitle.text = apodPictureItem.title
+            val formattedDate = apodPictureItem.date?.toDate()?.formatDate()
+            textViewPhotoDate.text = formattedDate ?: apodPictureItem.date
+            apodPictureItem.copyright?.let {
+                textViewPhotoCopyright.show()
+                textViewPhotoCopyright.text =
+                    textViewPhotoCopyright.context.getString(R.string.str_copyright, it)
+            } ?: kotlin.run {
+                textViewPhotoCopyright.hide()
+            }
+            apodPictureItem.url.let {
+                Glide.with(imageViewPlanet.context).load(it)
+                    .apply(RequestOptions.noTransformation())
+                    .error(R.drawable.ic_launcher_background)
+                    .listener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            progressBarView.progressBarView.hide()
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            progressBarView.progressBarView.hide()
+                            return false
+                        }
+                    })
+                    .into(imageViewPlanet)
+
+            }
+            textViewPhotoDetails.text = apodPictureItem.explanation
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_PictureDetailsFragment_to_PictureListingFragment)
-        }
+        observePicturesPosts()
     }
 
     override fun onDestroyView() {
