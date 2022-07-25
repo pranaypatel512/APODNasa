ed7498b498c97d5807c291bccdd0bb0b2a3373ed
pranaypatel512 <www.pranay1337.com@gmail.com>
2022-07-25 16:51:29 +0530

    :rotating_light: added test to check hours time difference

--- app/src/test/java/com/pranay/apodnasa/ExampleUnitTest.kt
+++ app/src/test/java/com/pranay/apodnasa/ExampleUnitTest.kt
@@ -1,8 +1,9 @@
 package com.pranay.apodnasa
 
+import java.time.Duration
+import java.time.LocalTime
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +12,17 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun time_different_of_hour_is_positive() {
+        val targetTime = LocalTime.of(2, 0, 0) //find diff for 6am using target hour time e.g 2am
+        val morningTime = LocalTime.of(6, 0, 0) //6am using target hour time of day
+        assertEquals(true, Duration.between(targetTime, morningTime).toHours() > 0)
+    }
+
+    @Test
+    fun time_different_of_hour_is_negative() {
+        val targetTime = LocalTime.of(8, 0, 0) //find diff for 6am using target hour time e.g 8am
+        //val targetTime = LocalTime.now() //find diff for 6am using target hour time e.g Current time
+        val morningTime = LocalTime.of(6, 0, 0) //6am using target hour time of day
+        assertEquals(true, Duration.between(targetTime, morningTime).toHours() < 0)
     }
 }
